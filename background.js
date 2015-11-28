c41afb83aa9733acce05e8d50840259733ce5cee
Sean Henderson <mail@sean.sh>
2015-11-27 23:35:25 -0800

    refactor magic to the background

--- background.js
+++ background.js
@@ -10,8 +10,23 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 // check the message from content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "selection_exists" ) {
-      // Do Something
+    if (request.message === "result_exists") {
+      // parse incoming html for markdown
+      var markdown = toMarkdown(request.content);
+      // create a ghost input to hold the fresh markdown
+      const textarea = document.createElement('textarea');
+      textarea.style.position = 'fixed';
+      textarea.style.opacity = 0;
+      // put da markdown in da box
+      textarea.value = markdown;
+      // put da box on the page
+      document.body.appendChild(textarea);
+      // select the newly appended text
+      textarea.select();
+      // copy it
+      document.execCommand('Copy');
+      // cover the tracks
+      document.body.removeChild(textarea);
     }
   }
 );
--- content.js
+++ content.js
@@ -1,49 +1,38 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.message === "clicked_browser_action") {
-            // get url of the current url
-            var page_location = window.location.href;
-            // get selection
-            var selection = window.getSelection();
-            // start out with the whole page
-            var body = document.getElementsByTagName("body");
-            // place to stash the html source
-            var html = document.createElement('html');
-            // check if user has highlighted any content
-            if (selection.type === "Range") {
-                // get the highlighted content's source
-                var range = selection.getRangeAt(0);
-                // stash it
-                html.appendChild(range.cloneContents());
-            } else {
-              // stash the entire <body>
-              html.appendChild(body[0]);
-            }
+  function(request, sender, sendResponse) {
 
-            if (html) {
-              console.log();
-              var markdown = toMarkdown(html.innerHTML)
+    if (request.message === "clicked_browser_action") {
+      // get url of the current url
+      var page_location = window.location.href;
+      // get selection
+      var selection = window.getSelection();
+      // start out with the whole page
+      var body = document.getElementsByTagName("body");
+      // place to stash the html source
+      var html = document.createElement('html');
+      // check if user has highlighted any content
+      if (selection.type === "Range") {
+          // get the highlighted content's source
+          var range = selection.getRangeAt(0);
+          // stash it
+          html.appendChild(range.cloneContents());
+        } else {
+        // stash the entire <body>
+        html.appendChild(body[0]);
+      }
 
-              // create a ghost input to hold the markdown and copy it
-              const textarea = document.createElement('textarea');
-              textarea.style.position = 'fixed';
-              textarea.style.opacity = 0;
-              textarea.value = markdown;
-              document.body.appendChild(textarea);
-              textarea.select();
-              document.execCommand('Copy');
-              document.body.removeChild(textarea);
+      if (html) {
+        // console.log(html.innerHTML);
+        // send a message to background.js
+        chrome.runtime.sendMessage({
+          "message": "result_exists",
+          "url": page_location,
+          "content": html.innerHTML
+        });
+      } else {
+        console.log("Oops - something ain't right....");
+      };
 
-              // send a message to background.js
-              chrome.runtime.sendMessage({
-                  "message": "selection_exists",
-                  "url": page_location,
-                  "content": html,
-                  "markdown": markdown
-              });
-            } else {
-              console.log("Oops - something ain't right....");
-            };
-        }
     }
+  }
 );
