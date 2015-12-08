0f4df0e06eb261d394c09631fc13f7bfc8aa7e7a
Sean Henderson <mail@sean.sh>
2015-12-08 01:21:25 -0800

    add secret markdown meme animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+jQuery.easing.def = "easeOutElastic";
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
 // live markdownizing demo
   // start with default html converted to md
@@ -10,5 +11,32 @@ $( document ).ready(function() {
   $('#toMarkdown_input').bind('change paste keyup', function() {
     $('#toMarkdown_output').val(toMarkdown($(this).val()));
   });
+
+// surpise demo unicorn
+  // set a unicorn counter
+  var unicount = 0;
+  // create a function for the animation
+  function animateSecret() {
+    $('#secret')
+      .animate({
+          top: "30%",
+          right: "40%"
+      }, 3000, 'swing' ).animate({
+          top: "-100%",
+          right: "-100%"
+      }, 5000, 'swing' )
+  }
+  // watch the user as they scroll... O.o
+  $(window).scroll(function() {
+    // make sure unicorn hasn't been spotted yet
+    if (unicount === 0) {
+      // check if demo section is visible
+      if ($('#demo').visible(true)) {
+        // hold a sec and start the show
+        setTimeout(animateSecret, 4000);
+      }
+    }
+  });
+
 
 });
