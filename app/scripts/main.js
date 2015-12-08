c46c39f7a02de72b2207308c6feacbf7569e73d2
Sean Henderson <mail@sean.sh>
2015-12-07 23:09:30 -0800

    live markdownizing demo
    (!!!!)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$( document ).ready(function() {
+
+// live markdownizing demo
+  // start with default html converted to md
+  var mdOut = toMarkdown($('#toMarkdown_input').val());
+  $('#toMarkdown_output').val(mdOut);
+  // watch the user. if they make any true moves, update the MD damnit!
+  $('#toMarkdown_input').bind('change paste keyup', function() {
+    $('#toMarkdown_output').val(toMarkdown($(this).val()));
+  });
+
+});
