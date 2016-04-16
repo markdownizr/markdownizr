e0f016b0826dab30cdcff35b35e65964bf7fbf44
Sean Henderson <mail@sean.sh>
2016-04-15 23:11:50 -0700

    add popover success message to current tab content

--- content.js
+++ content.js
@@ -24,6 +24,38 @@ chrome.runtime.onMessage.addListener(
         html.appendChild(documentFragment);
       }
 
+      // create a special div, just for markdownizr status updates
+      $( "body" ).append("<div id='markdownizr-status'></div>");
+
+      // function for showing the user what happened in markdownizr land
+      function statusMessage(status) {
+        console.log("status func called");
+        $("#markdownizr-status").css({
+          "display": "none",
+          "color": "white",
+          "font-family": "sans-serif",
+          "font-size": "24px",
+          "width": "20%",
+          "background": "rgba(0, 0, 0, .5)",
+          "border-radius": "10px",
+          "padding": "1em",
+          "position": "fixed",
+          "z-index": "1000000000", // a billion
+          // center it
+          "left": "50%",
+          "top": "20%",
+          "margin-left": "-10%" // Negative half of width.
+        });
+        // set the prose in the div depending on $argv[0]
+        if (status == "success") {
+          $("#markdownizr-status").text("Markdown Copied To Clipboard");
+        } else {
+          $("#markdownizr-status").text("Oops! Please Try Again");
+        }
+        // tell the status div to make an appearance, then leave
+        $( "#markdownizr-status" ).fadeIn(500).delay(1500);
+        $( "#markdownizr-status" ).fadeOut(500);
+      }
       if (html) {
         // console.log(html.innerHTML);
         // send a message to background.js
@@ -32,12 +64,14 @@ chrome.runtime.onMessage.addListener(
           "url": page_location,
           "content": html.innerHTML
         });
+        statusMessage("success");
       } else {
         chrome.runtime.sendMessage({
           "message": "no_result",
           "url": page_location,
           "content": html.innerHTML
         });
+        statusMessage("oops");
       };
 
     }
