8a914d7326662b89f0a70076a0ff52105f53b5c1
Sean Henderson <mail@sean.sh>
2018-05-29 20:11:45 -0700

    rename Filters group to Options

--- options_custom/manifest.js
+++ options_custom/manifest.js
@@ -3,28 +3,28 @@ this.manifest = {
     "icon": "icon.png",
     "settings": [
         {
-            "tab": "Filters",
+            "tab": "Options",
             "group": "Strip Elements",
             "name": "strip_elements",
             "type": "text",
             "text": "script, nav, p, ..."
         },
         {
-            "tab": "Filters",
+            "tab": "Options",
             "group": "Strip Elements",
             "name": "strip_desc",
             "type": "description",
             "text": "These elements will be removed from the generated markdown, but their content will be kept."
         },
         {
-            "tab": "Filters",
+            "tab": "Options",
             "group": "Delete Elements",
             "name": "delete_elements",
             "type": "text",
             "text": "script, nav, p, ..."
         },
         {
-            "tab": "Filters",
+            "tab": "Options",
             "group": "Delete Elements",
             "name": "delete_desc",
             "type": "description",
